6c7ef89d75f9fc20dcd31dbbd5554da96a1941b9
PADebin <71337635+PADebin@users.noreply.github.com>
2020-09-15 17:46:36 +0200

    Update and rename Site des Bonnes Nouvelles on Azure to accueil.php

--- views/accueil.php
+++ views/accueil.php
@@ -1,5 +1,5 @@
 <div id="contenu">
-	<h2>Accueil</h2>
+	<h2>Site des Bonnes Nouvelles on Azure</h2>
 	<p>Bienvenue aux séances d'exercices consacrées à élaborer un site Internet en PHP selon une architecture didactique MVC OO.</p>
 	<p>Jean-Luc Collinet.</p>
 </div><!-- #contenu -->
